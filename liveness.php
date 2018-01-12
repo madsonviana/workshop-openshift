7799b666327e716a1b2143a0b303d6b26706ece6
Madson Viana <me@madsonviana.info>
2018-01-12 17:42:24 +0000

    health check adicionado

--- liveness.php
+++ liveness.php
@@ -1,11 +1,9 @@
 <?php
+$filename = '/tmp/liveness';
 
-    $filename = '/tmp/liveness';
-
-    if(file.exists($filename)) {
-        header("HTTP/1.1 500 Internal Server Error");
-    } else {
-        echo "Ok";
-    }
-
+if (file_exists($filename)) {
+    header("HTTP/1.1 500 Internal Server Error");
+} else {
+    echo "Ok";
+}
 ?>
